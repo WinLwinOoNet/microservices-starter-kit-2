6c0f89e0649d2956cea52e91723b146bdc9a8584
Win Lwin Oo <winlwinoo@hotmail.com>
2020-09-17 20:13:06 -0700

    Complete Domain.Core

---
+++ src/Domain/Msk2.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Msk2.Domain.Core.Commands;
+using Msk2.Domain.Core.Events;
+
+namespace Msk2.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
---
+++ src/Domain/Msk2.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Msk2.Domain.Core.Events;
+
+namespace Msk2.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHander
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHander {}
+}
